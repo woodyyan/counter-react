f06ed9c2e8aeaefbce801ee204b67b5b052b3a91
Zhaoyu <weixiaobo88@gmail.com>
2020-04-18 17:08:01 +0800

    [Linne] - increase number by 1 once click plus button

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,15 +4,21 @@ class Counter extends Component {
   constructor(props) {
     super(props);
 
+    this.onIncrease = this.onIncrease.bind(this);
+
     this.state = {
       number: 10,
     };
   }
 
+  onIncrease() {
+    this.setState((prevState) => ({ number: prevState.number + 1 }));
+  }
+
   render() {
     return (
       <section>
-        <button>+</button>
+        <button onClick={this.onIncrease}>+</button>
         <mark>{this.state.number}</mark>
         <button>-</button>
       </section>
