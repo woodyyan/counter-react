17359d1a6e4af89d4dd1a443a709a939b91bcd8b
Zhaoyu <weixiaobo88@gmail.com>
2020-04-18 18:03:55 +0800

    [Linne] - generate a specific number of counter according to counterGroup props

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Counter from "./Counter";
+const uniqid = require("uniqid");
 
 class CounterGroup extends Component {
   render() {
+    const size = this.props.size;
+    const initValue = 0;
+    const countOfCounters = new Array(size).fill(initValue);
     return (
       <div>
-        <Counter />
+        {countOfCounters.map(() => (
+          <Counter key={uniqid()} />
+        ))}
       </div>
     );
   }
 }
 
-CounterGroup.propTypes = {};
+CounterGroup.propTypes = {
+  size: PropTypes.number.isRequired,
+};
 
 export default CounterGroup;
