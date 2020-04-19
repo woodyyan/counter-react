458e764f52993ed5abd4ac9099ed0bdee21faae3
Zhaoyu <weixiaobo88@gmail.com>
2020-04-19 08:01:52 +0800

    [Linne] - use the use input as counter size

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import uniqid from "uniqid";
 import Counter from "./Counter";
-const uniqid = require("uniqid");
 
 class CounterGroup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(event) {
+    this.setState({ number: event.target.value });
+  }
+
+  initArray(number) {
+    let size = this.props.size;
+    if (number.length > 0) {
+      size = parseInt(number);
+    }
+    return Array.from(Array(size).keys());
+  }
+
   render() {
-    const size = this.props.size;
-    const initValue = 0;
-    const countOfCounters = new Array(size).fill(initValue);
+    let countOfCounters = this.initArray(this.state.number);
+
     return (
       <div>
         <form>
           <fieldset>
             <label htmlFor="number">Generate </label>
-            <input name="number" placeholder="input number here..." />
+            <input
+              name="number"
+              placeholder="input number here..."
+              onChange={this.onChange}
+              value={this.state.size}
+            />
             <span> Counters</span>
           </fieldset>
         </form>
--- src/constants/constants.js
+++
@@ -1,3 +0,0 @@
-const INIT_ARRAY_VALUE = 0;
-
-export { INIT_ARRAY_VALUE };
